8eea2a93e52bec9f13c2b744ee4eefcb2f2d0463
junho <kode15333@gmail.com>
2022-05-26 00:31:44 +0900

    chore: remark plugin 추가

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,12 +1,14 @@
 import { remark } from 'remark'
-import html from 'remark-html'
 import prism from 'remark-prism'
+import remarkGfm from 'remark-gfm'
+import html from 'remark-html'
 import { VFileCompatible } from 'vfile'
 
 
 export default async function markdownToHtml(markdown: VFileCompatible) {
     const result =  await remark()
         .use(prism)
+        .use(remarkGfm)
         .use(html, { sanitize: false })
         .process(markdown);
     return result.toString()
