6e26f31c4815e63b140a5eb5d728dd72b301e8e6
junho <kode15333@gmail.com>
2022-06-07 01:14:06 +0900

    chore: add ga and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,28 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/ga'
 
 export default function Document() {
     return (
-        <Html lang="en">
-            <Head />
+        <Html lang='en'>
+            <Head>
+                <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', '${GA_TRACKING_ID}', {
+                                     page_path: window.location.pathname,
+                                    });
+                            `,
+                    }}
+                />
+            </Head>
             <body>
             <Main />
             <NextScript />
