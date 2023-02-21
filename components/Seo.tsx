3d1fa4d7f2c4922be5dd6f67e2a8aeb39307ad9f
junho <kode15333@gmail.com>
2023-02-21 22:52:22 +0900

    chore: sitemap 등록

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -2,20 +2,28 @@ import React from 'react'
 import Head from 'next/head'
 
 type Props = {
-    title: string,
+    title: string
     description: string
 }
 const Seo = ({ title, description }: Props) => {
     return (
         <Head>
             <title>{title}</title>
-            <meta name='description' content={description}/>
-            <meta name='op:title' content={title}/>
-            <meta name='op:description' content={description}/>
-            <meta name='op:type' content='website'/>
-            <meta name='twitter:card' content='summary'/>
-            <meta name='twitter:title' content={title}/>
-            <meta name='twitter:description' content={description}/>
+            <meta
+                content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1"
+                name="viewport"
+            />
+            <meta name="description" content={description} />
+            <meta name="op:title" content={title} />
+            <meta name="op:description" content={description} />
+            <meta name="op:type" content="website" />
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={description} />
+            <meta
+                name="naver-site-verification"
+                content="22974dbcbfc26d5faefe4284affa5c72b6f31c97"
+            />
         </Head>
     )
 }
